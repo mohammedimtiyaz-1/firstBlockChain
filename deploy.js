927ae4b5d57193af84192145ee45f6132fc8412f
Imtiyaz <mohammed.imtiyaz@decathlon.com>
2022-06-01 12:49:23 +0530

    deploys project

--- deploy.js
+++ deploy.js
@@ -1,1 +1,23 @@
 // deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "long any trumpet man utility name frame helmet canyon beauty call round",
+  "https://rinkeby.infura.io/v3/44e5a45a32eb4befa97a3db8411dcecd"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("list og accounts", accounts[0]);
+  const res = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+  console.log("deployed address: ", res.options.address);
+  provider.engine.stop();
+};
+
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,16 +7,24 @@ const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
-beforeEach(async (done) => {
+beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ["Hi Hello"] })
     .send({ from: accounts[0], gas: "1000000" });
-  done();
 });
 
 describe("Inbox test", () => {
   it("deployes contract", () => {
-    // console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi Hello");
+  });
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "bye");
   });
 });
